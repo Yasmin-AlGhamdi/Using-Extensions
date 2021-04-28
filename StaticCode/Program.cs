bfff3a8f87b507ffa16d7a99ed51e5b85794d46a
Yasmin-AlGhamdi <82459882+Yasmin-AlGhamdi@users.noreply.github.com>
2021-04-28 14:54:12 +0300

    Add files via upload

---
+++ StaticCode/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace StaticCode
+{
+    public static class CodeHelpers
+    {
+        public static string trim(string code)
+        {
+            return null;
+        }
+        public static int doAction(string code)
+        {
+            return 0;
+        }
+    }
+    public static class MyHelper
+    {
+        public static bool isCapitalized(this string s) // Extension: this string s
+        {
+            return char.IsUpper(s[0]);
+        }
+        public static bool isOdd(this int a)
+        {
+            return a % 2 != 0;        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Welcome to C#";
+            if(message.isCapitalized()) // message is string so no need to put it inside isCapitalized
+            {
+                Console.WriteLine("Capitalized");
+            }
+            else
+            {
+                Console.WriteLine("Not Capitalized");
+            }
+            // StringHelper.isCapitalized(message);
+
+            /*
+            int age = 21;
+            if(age.isOdd())
+            {
+                Console.WriteLine(age);
+            }
+            */
+
+            //xxutil (in java)
+            string s = CodeHelpers.trim(" Welcome to C#  ");
+            //Console.WriteLine(s);
+            Math.Floor(4.5);
+            Math.Ceiling(4.5);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ test4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace test4
+{
+    public static class MyHelper
+    {
+        public static int isdoble(this int val)
+        {
+            return val * 2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v = 2;
+            int v1 = v.isdoble();
+            int v2 = v1.isdoble();
+            Console.WriteLine(v.isdoble());
+            Console.WriteLine(v1.isdoble());
+            Console.WriteLine(v2.isdoble());
+        }
+    }
+}
